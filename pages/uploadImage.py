2bdf6e202042a3e6bd2f87f41538cf36f86db805
SarahPauwels-ucll <r0875579@ucll.be>
2025-05-14 10:29:30 +0200

    errorhandeling upload image

--- pages/uploadImage.py
+++ pages/uploadImage.py
@@ -1,43 +1,44 @@
 from st_pages import Page, add_page_title
 import streamlit as st
 import os
-
-
+   
 st.set_page_config(page_title="Upload image",
-                   page_icon=":books:",
                    layout="wide")
 
+if "upload_errors" not in st.session_state:
+    st.session_state["upload_errors"] = []
 
- 
 def upload_files():
+    st.session_state["upload_errors"] = []
     files = st.session_state["uploaded_files"]
 
+    if files==[]or files==None:
+        st.session_state["upload_errors"].append("No files uploaded")
     for file in files:
         print("processing file: ", file)
         filename = file.name
         name, ext = os.path.splitext(filename)
         ext = ext.replace('.', '')
-        if ext == 'jpg':
-            file_size = file.size
-            if file_size / 1e6 < 25:
 
+        if ext == 'jpg':
                 os.makedirs("image", exist_ok=True)
                 with open(os.path.join("image","image.jpg"),"wb") as f:
                     f.write(file.getbuffer())
-                
-            else: # should show warning
-                print(f"Cannot use files with extension '{ext}' with size bigger than 25MB.")
+                st.session_state["upload_errors"].append(f"File '{name}' is uploaded successfully")
+
         else:
-             print(f"Cannot use files with extension '{ext}'")
-        
-            
+             st.session_state["upload_errors"].append(f"Cannot use files with extension '{ext}' use 'jpg' instead")
 
-st.markdown(" # Upload Data")
-
-# upload files 
+# layout
 st.header("Upload Dental image")
-st.error("Please ensure the image is an jpg")
+st.error("Please ensure the image is an 'jpg'")
 with st.container(border=True):
     files = st.file_uploader("Image uploader", accept_multiple_files=True, key="uploaded_files")
 
     st.button("Upload image", on_click=upload_files)
+
+for message in st.session_state.get("upload_errors", []):
+    if "successfully" in message:
+        st.success(message)
+    else:
+        st.error(message)
