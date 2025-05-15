ceef106ff29dfd2ec75d90218fab197761343abe
SarahPauwels-ucll <r0875579@ucll.be>
2025-05-15 14:41:55 +0200

    comparison page

--- pages/Comparison.py
+++ pages/Comparison.py
@@ -1,6 +1,8 @@
 from st_pages import Page, add_page_title
 import streamlit as st
 from sidebar import load_sidebar
+from teeth import load_teeth
+from input.teethSet import teeth
 import os
    
 st.set_page_config(page_title="comparison",
@@ -8,3 +10,26 @@ st.set_page_config(page_title="comparison",
 
 
 load_sidebar()
+
+st.title("Comparison page!")
+ai_image_path = os.path.join("AIOutput", "image.jpg")
+image_path=os.path.join("image", "image.jpeg")
+# Check if the image exists
+if os.path.exists(ai_image_path) and os.path.exists(image_path) :
+    st.markdown("""
+    <style>
+    .st-key-photo-container {
+        margin: 0 auto;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+    with st.container(key="photo-container"):
+        cols = st.columns(2)
+        with cols[0]:
+            st.image(image_path,  use_container_width=True)
+        with cols[1]:
+            st.image(ai_image_path,  use_container_width=True)
+else:
+    st.warning("No image has been uploaded yet.")
+
+load_teeth(teeth)
