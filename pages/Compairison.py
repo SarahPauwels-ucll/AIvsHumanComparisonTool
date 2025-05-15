aed2e9fb853e9c9647c3b59f28491b101cf18bf2
SarahPauwels-ucll <r0875579@ucll.be>
2025-05-15 09:00:35 +0200

    compairison page

---
+++ pages/Compairison.py
@@ -0,0 +1,10 @@
+from st_pages import Page, add_page_title
+import streamlit as st
+from sidebar import load_sidebar
+import os
+   
+st.set_page_config(page_title="compairison",
+                   layout="wide")
+
+
+load_sidebar()
