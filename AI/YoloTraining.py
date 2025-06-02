8bf87f8116818b1647318d45ef73766830ec9ce9
Jarne <jarne.theys@gmail.com>
2025-06-02 16:22:35 +0200

    add cuda support

--- AI/YoloTraining.py
+++ AI/YoloTraining.py
@@ -1,4 +1,8 @@
+from multiprocessing import freeze_support
 from ultralytics import YOLO
 
-model = YOLO("runs/detect/train/weights/best.pt")  # Start from pretrained nano model
-model.train(data="AI/yolo_teeth.yaml", epochs=50, imgsz=640, batch=8)
+if __name__ == "__main__":
+    freeze_support()
+    model = YOLO("runs/detect/train/weights/best.pt")
+    model.to("cuda")
+    model.train(data="AI/yolo_teeth.yaml", epochs=500, imgsz=640, batch=8)
