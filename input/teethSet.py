166e47fc9f0d2445f919be3f041cdb72aa87e885
SarahPauwels-ucll <r0875579@ucll.be>
2025-06-02 10:57:35 +0200

    set to missing

--- input/teethSet.py
+++ input/teethSet.py
@@ -11,3 +11,17 @@ childteeth={
     71:None, 72:None, 73:None, 74:None, 75:None,
     81:None, 82:None, 83:None, 84:None, 85:None,
 }
+
+missingteeth={
+    11:"missing", 12:"missing", 13:"missing", 14:"missing", 15:"missing", 16:"missing", 17:"missing", 18:"missing",
+    21:"missing", 22:"missing", 23:"missing", 24:"missing", 25:"missing", 26:"missing", 27:"missing", 28:"missing",
+    31:"missing", 32:"missing", 33:"missing", 34:"missing", 35:"missing", 36:"missing", 37:"missing", 38:"missing",
+    41:"missing", 42:"missing", 43:"missing", 44:"missing", 45:"missing", 46:"missing", 47:"missing", 48:"missing"
+}
+
+missingchildteeth={
+    51:"missing", 52:"missing", 53:"missing", 54:"missing", 55:"missing",
+    61:"missing", 62:"missing", 63:"missing", 64:"missing", 65:"missing",
+    71:"missing", 72:"missing", 73:"missing", 74:"missing", 75:"missing",
+    81:"missing", 82:"missing", 83:"missing", 84:"missing", 85:"missing",
+}
