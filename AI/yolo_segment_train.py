fd6935421203c634d0ca6991f52ad618c73f5b30
Jarne <jarne.theys@gmail.com>
2025-06-03 14:22:09 +0200

    add finetuned model to github

--- AI/yolo_segment_train.py
+++ AI/yolo_segment_train.py
@@ -4,11 +4,11 @@ import torch
 
 if __name__ == "__main__":
     freeze_support()
-    model = YOLO("yolo11m-seg.pt")
+    model = YOLO("runs/segment/train/weights/best.pt")
     if torch.cuda.is_available():
         model.to("cuda")
         gpu_name = torch.cuda.get_device_name(0)
         print(f"Using GPU {gpu_name} for training")
     else:
         print("CUDA not detected, using CPU for training")
-    model.train(data="AI/yolo_segments.yaml", epochs=50, imgsz=640, batch=8)
+    model.train(data="AI/yolo_segments.yaml", epochs=500, imgsz=640, batch=8)
